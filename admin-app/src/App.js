25a921072c62dc1287e7bff087274f4d89cb282d
unknown <salman.alam@g.bracu.ac.bd>
2022-07-29 21:24:58 +0600

    Commit

---
+++ admin-app/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import './App.css';
+import Home from './containers/Home';
+import Signin from './containers/Signin';
+import Signup from './containers/Signup';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" exact element={<Home/>} />
+          <Route path="/Signin" element={<Signin/>} />
+          <Route path="/Signup" element={<Signup/>} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ admin-app/src/containers/Home/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Layout from '../../components/Layout'
+
+/**
+* @author
+* @function Home
+**/
+
+export const Home = (props) => {
+  return(
+    <Layout>
+      
+    </Layout>
+   )
+
+ }
+
+ export default Home
