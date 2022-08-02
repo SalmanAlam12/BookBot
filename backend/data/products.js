68eb6bfcdd27c516106b31e7fafbf367020b5f62
unknown <salman.alam@g.bracu.ac.bd>
2022-08-03 02:07:42 +0600

    Convert to ES modules

--- backend/data/products.js
+++ backend/data/products.js
@@ -28,4 +28,4 @@ const products = [
   },
 ]
 
-module.exports = products
+export default products
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,10 @@
-const express = require('express')
+import express from 'express'
+import dotenv from 'dotenv'
+import products from './data/products.js'
+
+dotenv.config()
+
 const app = express()
-const products = require('./data/products')
 
 app.get('/', (req, res) => {
   res.send('API running......')
@@ -15,4 +19,9 @@ app.get('/api/products/:id', (req, res) => {
   res.json(product)
 })
 
-app.listen(5000, console.log('Server running on port 5000'))
+const PORT = process.env.PORT || 5000
+
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`)
+)
