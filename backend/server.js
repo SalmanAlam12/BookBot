087997e9af67f76013226a2446a30767a1c7298b
unknown <salman.alam@g.bracu.ac.bd>
2023-06-12 23:26:56 +0600

    Deployment Stage

--- backend/server.js
+++ backend/server.js
@@ -16,10 +16,6 @@ const app = express()
 
 app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.send('API running......')
-})
-
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
@@ -27,6 +23,18 @@ app.use('/api/upload', uploadRoutes)
 
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
+if (process.env.NODE_ENV == 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+  app.get('*', (req, res) =>
+    res.sendfile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API running......')
+  })
+}
 
 app.use(notFound)
 app.use(errorHandler)
